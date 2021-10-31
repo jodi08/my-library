878295efecf736b49a0e4bc745cd5da369d18c0b
Jo Anna Mollman <mommollman@gmail.com>
2021-10-31 07:01:09 -0700

    begin new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import List from './components/Library'
 
 function App() {
+  const [book, setBook] = useState('');
+  const [list, setList] =  useState([]);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log('it clicked')
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="book-container">
+      <h1>Custom Library</h1>
+      <form className='form'>
+        <label htmlFor="">Book Name: </label>
+        <input className="book-name" type="text" />
+        <label htmlFor="">Author: </label>
+        <input type="text" />
+        <label htmlFor="">Genre: </label>
+        <input type="text" />
+        <button type="submit" className='btn' onClick={handleSubmit}>
+          click me
+        </button>
+      </form>
+      <ul>
+        <li>{book}</li>
+      </ul>
     </div>
+  
+    
   );
 }
 
---
+++ src/components/Library.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default List
